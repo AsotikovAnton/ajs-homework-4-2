a6365817345899ef20c2ac5ea3f7cc45a1d6e0bf
Asotikov Anton <asant@yandex.ru>
2019-12-07 18:54:37 +0300

    Исправил задачу 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 // TODO: write your code here
-export default function findBy(fld, val) {
-  const field = fld;
-  const value = val;
-  return function (element) {
+const findBy = (field, value) => {
+  return element => {
     if (element[field].includes(value)) return true;
     return false;
   };
 }
+
+export default findBy;
